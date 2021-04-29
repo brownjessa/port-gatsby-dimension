6138f111ab094327c637786f80c59528ae571e0c
Jessamine Brown <brownjessa11@gmail.com>
2021-04-29 14:27:19 -0500

    update content

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,13 +33,12 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            An innovative and solution-focused UX/UI, Product Designer and fledgling front-end developer. 
+            My passion is for technology and designing responsive 2D products as well as 3D enviornments 
+            and products.
+            Check out my <a href="#">Resume</a>.
           </p>
-          <p>
+          {/* <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
             dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
             torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
@@ -51,7 +50,7 @@ class Main extends React.Component {
             malesuada fames ac turpis egestas. In non lorem sit amet elit
             placerat maximus. Pellentesque aliquam maximus risus, vel sed
             vehicula.
-          </p>
+          </p> */}
           {close}
         </article>
 
@@ -67,19 +66,54 @@ class Main extends React.Component {
             <img src={pic02} alt="" />
           </span>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+          <header> 
+          <h2>Member Dashboard</h2>
+        <section>
+          <ul className ="alt">
+										<li><a href="https://xd.adobe.com/view/31e9a0aa-23bf-46f0-80ba-1cdafdf45095-64ba/">Dark Theme</a></li>
+										<li><a href="https://xd.adobe.com/view/31e9a0aa-23bf-46f0-80ba-1cdafdf45095-64ba/">Light Theme</a></li>
+									</ul>
+          </section>
+          </header>
+
+          <header className="major"> 
+          <h2>Hierarchy User Flows</h2>
+        <section>
+          <ul className="alt">
+										<li><a href="https://drive.google.com/file/d/1aCYT1qtywtGtIPlszj45kEUPBnLBGiD2/view?usp=sharing">Super Admin</a></li>
+										<li><a href="https://drive.google.com/file/d/1dNG13jUipryqD3i5dEmJvkQH9RwGmqvN/view?usp=sharing">Admin</a></li>
+                    <li><a href="https://drive.google.com/file/d/1Mxf---rD1xtY2dbSu7VUQ8JejYRFS0ji/view?usp=sharing">Standard Users</a></li>
+									</ul>
+          </section>
+          </header>
+
+          <header> 
+          <h2>Cyber Data Viz</h2>
+          <div className="content">
+        <section>
+          <ul className="alt">
+										<li><a href="https://xd.adobe.com/view/6c1ac636-6876-4394-425b-fd3ca5fe00e7-1a2b/">Mock</a></li>
+										<li><a href="https://xd.adobe.com/view/595ee5d9-8d4a-484b-4929-4711c61da7d1-2f6f/">Styling Guide</a></li>
+                    <li><a href="https://xd.adobe.com/view/c0bcb6bc-a525-4242-423f-63f29fc07bc9-b68d/">Styling Edit</a></li>
+									</ul>
+                  <p>For password to view these mocks please contact me.</p>
+          </section>
+          </div>
+          </header>
+
+          <header> 
+          <h2>3D--VR Design</h2>
+        <section>
+          <ul className="alt">
+										<li><a href="https://hubs.mozilla.com/scenes/Hb96YHJ">Surrounded Lake VR Hub</a></li>
+                    <li><a href="https://drive.google.com/file/d/1fEbHqEP7v-Vhn7bjy0wqefB5VgmGFZ8b/view">2D/3D Webex Conference Room</a></li>
+									</ul>
+                  <p>Please access via Desktop using Google Chrome</p>
+          </section>
+          </header>
+
+
+
           </p>
           {close}
         </article>
@@ -96,13 +130,11 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            I enjoy photography and using Adobe suite to create mixed media art
+            to sell as NFT's. I also design and create japanese tie-dye shirts with patches that signal an 
+            Augmented Reality animation. Currently I am reading Creative Strategy and the Business of Design, 
+            Powered By Design, Man's Search for Meaning, and The I Ching. I believe in living holistically, 
+            the love of dogs, video games, and that small acts of kindness create peace globally.
           </p>
           {close}
         </article>
@@ -115,7 +147,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          {/* <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
@@ -136,29 +168,33 @@ class Main extends React.Component {
                 <input type="reset" value="Reset" />
               </li>
             </ul>
-          </form>
+          </form> */}
+          <p>Jessamine Brown
+            <br></br>
+            brownjessa11@gmail.com
+          </p>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/jessrawrmine"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
+              <a href="https://www.linkedin.com/in/jessamine-brown-6a4789128/" className="icon fa-linkedin">
+                <span className="label">LinkedIn</span>
               </a>
             </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+            {/* <li>
+              <a href="#" className="icon fa-instagram">
                 <span className="label">Instagram</span>
               </a>
-            </li>
+            </li> */}
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/brownjessa"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
